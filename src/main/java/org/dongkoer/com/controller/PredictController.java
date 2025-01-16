2c4db8cacc930b03dd566a569491b1b9745f7b97
dongkoer <dongkeer@hosecloud.com>
2025-01-16 18:10:53 +0800

    update

--- src/main/java/org/dongkoer/com/controller/PredictController.java
+++ src/main/java/org/dongkoer/com/controller/PredictController.java
@@ -1,6 +1,6 @@
 package org.dongkoer.com.controller;
 
-import com.alibaba.fastjson2.JSON;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import okhttp3.MediaType;
@@ -9,6 +9,8 @@ import okhttp3.Request;
 import okhttp3.Response;
 import org.dongkoer.com.entity.Predict;
 import org.dongkoer.com.entity.ResponseFromPython;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +21,10 @@ import java.net.URLDecoder;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.TypeReference;
 
 @RestController
 @RequestMapping("/api/predict")
@@ -43,8 +49,15 @@ public class PredictController {
             Response response = client.newCall(postRequest).execute();
             String stringcode = response.body().string();
 //            String s = decodeUnicode(stringcode);
+            stringcode = decodeUnicode(stringcode);
+            System.out.println(stringcode);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(org.springframework.http.MediaType.APPLICATION_JSON_UTF8);
 
-            return ResponseEntity.ok(stringcode);
+
+
+            return new ResponseEntity<>(stringcode, headers, HttpStatus.OK);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -53,12 +66,34 @@ public class PredictController {
 
 
     private String decodeUnicode(String unicodeString) {
-        try {
-            return URLDecoder.decode(unicodeString, StandardCharsets.UTF_8.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-            return unicodeString;
+
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+            while (i < unicodeString.length()) {
+                char c = unicodeString.charAt(i++);
+                if (c == '\\' && i < unicodeString.length() && unicodeString.charAt(i) == 'u') {
+                    i++;
+                    int value = 0;
+                    for (int j = 0; j < 4 && i < unicodeString.length(); j++, i++) {
+                        char ch = unicodeString.charAt(i);
+                        if (ch >= '0' && ch <= '9') {
+                            value = (value << 4) + ch - '0';
+                        } else if (ch >= 'a' && ch <= 'f') {
+                            value = (value << 4) + 10 + ch - 'a';
+                        } else if (ch >= 'A' && ch <= 'F') {
+                            value = (value << 4) + 10 + ch - 'A';
+                        } else {
+                            throw new IllegalArgumentException("Malformed \\uxxxx encoding.");
+                        }
+                    }
+                    sb.append((char) value);
+                } else {
+                    sb.append(c);
+                }
+            }
+            return sb.toString();
         }
+
     }
-}
+
 
